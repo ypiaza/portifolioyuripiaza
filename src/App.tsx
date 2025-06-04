24ebc9872931cfe5e21639513fd43eb857ad5432
ypiaza <yuripiazagomessantos@gmail.com>
2025-06-04 08:21:10 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Main from "./components/Main"
 
 const App = () => {
   return (
-    <div className="px-20 py-4">
+    <div className="px-12 py-2">
       <Apresentation />
       <Main />
       <Footer />
