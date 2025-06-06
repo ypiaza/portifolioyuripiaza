88dde29cbb1e1bb1d02106db433823a857624319
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-06-06 13:32:11 -0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react"
 import { menuFooter } from "../data/menu"
 import AvaliableForJob from "./AvaliableForJob"
+import { CgClose } from "react-icons/cg"
+import { RiMenu4Fill } from "react-icons/ri";
+
 
 const Footer = () => {
+  const [btnFloat, setBtnFloat] = useState(false)
+
   return (
-    <div className='fixed z-50 bottom-2 md:flex items-center justify-center md:justify-between w-[96%] md:w-[91.7%] px-12 py-5 rounded-3xl border border-white/10 bg-[#202020]/50 backdrop-blur-md mt-2'>
+    <div className='fixed z-50 bottom-2 md:flex items-center justify-center md:justify-between md:w-[91.7%] px-5 md:px-12 py-5 rounded-3xl border border-white/10 bg-[#202020]/50 backdrop-blur-md mt-2'>
+
+      <RiMenu4Fill onClick={()=>setBtnFloat(true)} className={`text-5xl md:text-4xl text-white ${btnFloat ? 'hidden' : 'block'}`} />
+
+
       <div>
-        <ul className="flex items-center justify-center gap-3 ">
+        <ul className={`flex-col md:flex-row items-center justify-center gap-5 md:gap-10 transition-all duration-200 ${btnFloat === false ? 'hidden' : 'flex'}`}>
+          <li onClick={() => setBtnFloat(false)} className={`text-5xl md:text-4xl mt-10 md:mt-0 text-white list-none ${btnFloat === false ? 'hidden' : 'block'}`}><CgClose /></li>
           {menuFooter.map((item) => (
-            <li><a target="_blank" href={item.link}><item.icon className='text-white text-2xl hover:text-blue-500' /></a></li>
+            <li><a target="_blank" href={item.link}><item.icon className='text-white text-5xl md:text-4xl hover:text-blue-500' /></a></li>
           ))}
         </ul>
       </div>
