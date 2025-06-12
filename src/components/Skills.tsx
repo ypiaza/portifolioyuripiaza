b2fe047cd94f61b5f1fe06553825014ddfa213f4
ypiaza <yuripiazagomessantos@gmail.com>
2025-06-12 14:22:50 -0300

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,7 +7,7 @@ const Skills = () => {
   return (
     <div className='hidden md:grid md:grid-cols-4 md:h-[15rem] overflow-hidden rounded-2xl md:rounded-3xl md:border text-white md:border-white/10 bg-transparent backdrop-blur-sm shadow-2xl shadow-black/30'>
       {skills.map((item: itemSkill, index) => (
-        <div className={`relative text-4xl md:border md:border-white/10 flex items-center justify-center overflow-hidden cursor-pointer transition-all duration-300 bg-blue-500/50 animate-pulse delay-500`}
+        <div className={`relative text-4xl md:border md:border-white/10 flex items-center justify-center overflow-hidden cursor-pointer transition-all duration-300 bg-blue-500/50 animate-pulse delay-500`}>
         onMouseOver={() => setHoverIcon(index)}
         onMouseOut={() => setHoverIcon(false)}>
           <item.icon className='z-50'/>
