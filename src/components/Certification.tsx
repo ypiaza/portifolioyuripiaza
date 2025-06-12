e947f428f5aa0a96cfc2790049194b5fd956404b
ypiaza <yuripiazagomessantos@gmail.com>
2025-06-12 13:48:50 -0300

    Update Certification.tsx

--- src/components/Certification.tsx
+++ src/components/Certification.tsx
@@ -3,13 +3,13 @@ import { certification } from "../data/certification"
 const Certification = () => {
   return (
     <div className=" flex flex-col items-start h-full px-12 py-14 rounded-3xl border border-white/10 bg-transparent backdrop-blur-sm shadow-2xl shadow-black/30">
-        <div className="text-white/40 mb-10 font-bold">Certificados:</div>
+        <div className="text-white/70 mb-10 font-bold">Certificados:</div>
         <div className="w-full">
             <ul>
                 {certification.map((item) => (
                     <li className='mb-5 opacity-90 w-full flex justify-between flex-col'>
-                        <p className="font-bold text-blue-500/70">{item.title}</p>
-                        <p className="text-white/40 text-sm">{item.about}</p>
+                        <p className="font-bold text-blue-500/80">{item.title}</p>
+                        <p className="text-white/70 text-sm">{item.about}</p>
                     </li>
                 ))}
             </ul>
