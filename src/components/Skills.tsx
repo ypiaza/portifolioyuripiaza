4fe853a7efe576a3bdd495ac2e0987293965d4dc
ypiaza <yuripiazagomessantos@gmail.com>
2025-06-12 14:29:12 -0300

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import { skills, type itemSkill } from "../data/skills"
 
 const Skills = () => {
+  const [hoverIcon, setHoverIcon] = useState<number | null>(null)
 
   return (
     <div className='hidden md:grid md:grid-cols-4 md:h-[15rem] overflow-hidden rounded-2xl md:rounded-3xl md:border text-white md:border-white/10 bg-transparent backdrop-blur-sm shadow-2xl shadow-black/30'>
       {skills.map((item: itemSkill, index) => (
         <div
           key={index}
-          className='relative text-4xl md:border md:border-white/10 flex items-center justify-center overflow-hidden cursor-pointer transition-all duration-300 bg-blue-500/50 animate-pulse delay-500'>
+          onMouseOver={() => setHoverIcon(index)}
+          onMouseOut={() => setHoverIcon(null)}
+          className={`relative text-4xl md:border md:border-white/10 flex items-center justify-center overflow-hidden cursor-pointer transition-all duration-300 ${
+            hoverIcon === index ? 'bg-blue-700/70' : 'bg-blue-500/50'
+          }`}
+        >
           <item.icon className='z-50' />
         </div>
       ))}
