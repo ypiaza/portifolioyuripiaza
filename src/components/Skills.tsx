a9db90d5cad3bb7d2b5a239fe35b8fd084dba552
ypiaza <yuripiazagomessantos@gmail.com>
2025-06-12 14:31:19 -0300

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,21 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { skills, type itemSkill } from "../data/skills"
 
 const Skills = () => {
-  const [hoverIcon, setHoverIcon] = useState<number | null>(null)
+  const [activeIndex, setActiveIndex] = useState<number | null>(null)
+
+  useEffect(() => {
+    let index = 0
+    const interval = setInterval(() => {
+      setActiveIndex(index)
+      index = (index + 1) % skills.length // reinicia quando chega no final
+    }, 500)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className='hidden md:grid md:grid-cols-4 md:h-[15rem] overflow-hidden rounded-2xl md:rounded-3xl md:border text-white md:border-white/10 bg-transparent backdrop-blur-sm shadow-2xl shadow-black/30'>
       {skills.map((item: itemSkill, index) => (
         <div
           key={index}
-          onMouseOver={() => setHoverIcon(index)}
-          onMouseOut={() => setHoverIcon(null)}
           className={`relative text-4xl md:border md:border-white/10 flex items-center justify-center overflow-hidden cursor-pointer transition-all duration-300 ${
-            hoverIcon === index ? 'bg-blue-700/70' : 'bg-blue-500/50'
+            activeIndex === index ? 'bg-blue-700/70 scale-110' : 'bg-blue-500/20 scale-100'
           }`}
         >
-          <item.icon className='z-50' />
+          <item.icon className='z-50 transition-all duration-300' />
         </div>
       ))}
     </div>
