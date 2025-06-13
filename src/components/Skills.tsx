5b1cb8ea04d0095237cb3b55c3276fa4fcc409fe
ypiaza <yuripiazagomessantos@gmail.com>
2025-06-13 14:07:15 -0300

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,12 +3,11 @@ import { skills, type itemSkill } from "../data/skills"
 const Skills = () => {
 
   return (
-    <div className='hidden md:grid md:grid-cols-8 p-[0.1rem] overflow-hidden text-transparent glass rounded-3xl shadow-2xl shadow-black/30 '>
+    <div className='hidden md:grid md:grid-cols-8 p-[0.1rem] overflow-hidden text-transparent glass rounded-3xl shadow-2xl shadow-black/30'>
       {skills.map((item: itemSkill, index) => (
         <div
           key={index}
-          className={`text-4xl flex items-center p-5 justify-center}
-        >
+          className={`text-4xl flex items-center p-5 justify-center`} >
           <item.icon className={`z-50 text-white`} />
         </div>
       ))}
